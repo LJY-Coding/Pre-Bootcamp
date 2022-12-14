eec1cdc0e35e8d8542efd87003dcdab13c7add0b
Jacob Yang <jacob.yang@gordon.edu>
2022-12-13 22:04:17 -0500

    initial commit

---
+++ Yang_Jacob_JS_Tinkering.js
@@ -0,0 +1,8 @@
+var childHeight = 6;
+function displayIfChildIsAbleToRideTheRollerCoaster(childHeight){
+    if (childHeight > 52){
+        console.log("Get on that ride, kiddo!");
+    } else{
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+}
